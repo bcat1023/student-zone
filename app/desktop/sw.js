0330226ea3b6511362649dbc7902ee467cb9ed2d
bcat <63063627+bcat1023@users.noreply.github.com>
2020-12-01 15:28:23 -0800

    Update sw.js

--- app/desktop/sw.js
+++ app/desktop/sw.js
@@ -1,13 +1,26 @@
-importScripts('/cache-polyfill.js');
+const version = "0.6.19";
+const cacheName = `airhorner-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
 
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       'offline.html',
-       'index.html',
-     ]);
-   })
- );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
